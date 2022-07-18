82cafcf5674a58591fb8c5cb6255d5709c781cac
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-07-18 07:31:30 -0600

    added file loader

--- assets/js/domMethods.js
+++ assets/js/domMethods.js
@@ -1,53 +1,37 @@
-$(document).ready(function() {
-    function createEl(htmlString, attrs, ...children) {
-      if (typeof htmlString !== "string") {
-        throw Error("Argument 'htmlString' is required and must be a string");
-      }
-    
-      const el = document.createElement(htmlString);
-    
-      if (typeof attrs === "object") {
-        for (let key in attrs) {
-          if (key.substring(0, 2) === "on") {
-            el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-          } else {
-            el.setAttribute(key, attrs[key]);
-          }
+function createEl(htmlString, attrs, ...children) {
+  if (typeof htmlString !== "string") {
+    throw Error("Argument 'htmlString' is required and must be a string");
+  }
+
+  const el = document.createElement(htmlString);
+
+  if (typeof attrs === "object") {
+    for (let key in attrs) {
+      if (key.substring(0, 2) === "on") {
+        el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+      } else if (key === "style") {
+        for (let rule in attrs[key]) {
+          el.style[rule] = attrs[key][rule];
         }
+      } else {
+        el.setAttribute(key, attrs[key]);
       }
-    
-      children.forEach(function(child) {
-        let node;
-    
-        if (child.constructor.name.includes("Element")) {
-          node = child;
-        } else {
-          node = document.createTextNode(child);
-        }
-    
-        el.appendChild(node);
-      });
-    
-      return el;
-    };
-  
-     
-  
-      const pageEl = document.querySelector("#page");
-      
-      const containerEl = createEl("div", {class: "container"},
-        createEl("div", {class: "card mb-3"}, 
-          createEl("img", {class: "card-img-top", style: "width: 5px", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
-          createEl("div", {class: "card-body"}, 
-            createEl("h1", {class: "card-title"}, currentEvent.title || ""),
-            createEl("h2", {class: "text-muted"}, currentEvent.subtitle || ""),
-            createEl("p", {class: "card-text mt-3"}, currentEvent.description || createLoremIpsum(100)),
-            createEl("a", {class: "btn btn-primary", href: "tickets.html"}, "Buy Tickets")
-          )
-        ),
-        
-      )
-      pageEl.appendChild(containerEl)
+    }
+  }
+
+  children.forEach(function(child) {
+    let node;
+
+    if (child.constructor.name.includes("Element")) {
+      node = child;
+    } else {
+      node = document.createTextNode(child);
     }
 
-    module.exports = createEl;
+    el.appendChild(node);
+  });
+
+  return el;
+}
+
+module.exports = createEl;
--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,6 +1,9 @@
+require('bootstrap');
+const createEl = require("./domMethods");
 const { createLoremIpsum, dateConverter } = require("./helpers");
 
 $(document).ready( function() {
+    if (window.location.href.indexOf("schedule") > -1) {
 
     const date = new Date();
     const d = date.getDate();
@@ -8,7 +11,6 @@ $(document).ready( function() {
     const y = date.getFullYear();
 
     function onEventClick(calEvent) {
-
     const start = dateConverter(calEvent.start);
     localStorage.setItem("currentEvent", JSON.stringify({
         title: calEvent.title,
@@ -119,3 +121,4 @@ $(document).ready( function() {
     pageEl.appendChild(containerEl2);
     pageEl.appendChild(containerEl3);
   }
+});
